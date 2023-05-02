f96fea3b8d154a1485a83a944d1218013f16a7c1
NVrhovski <neyenvrhovski@gmail.com>
2023-05-02 13:23:09 +0200

    add-modal

--- components/ServicesCard/index.tsx
+++ components/ServicesCard/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import styles from './ServicesCard.module.scss'
 
 interface IPropsServicesCard {
@@ -9,6 +9,7 @@ interface IPropsServicesCard {
 const ServicesCard = ({image, title}: IPropsServicesCard) => {
 
     const shadowRef = useRef<any>(null);
+    const [showModal, setShowModal] = useState(false);
 
     const toggleDescription = (option: boolean): void => {
         if(option)
@@ -22,15 +23,27 @@ const ServicesCard = ({image, title}: IPropsServicesCard) => {
     }
 
     return(
-        <div className={styles.serviceItem} onMouseOver={(() => {toggleDescription(true)})} onMouseLeave={(() => {toggleDescription(false)})}>
-            <img src={image}/>
-            <div className={styles.itemFooter}>
-                <span>{title}</span>
-            </div> 
-            <div className={styles.serviceDescriptionContainer} ref={shadowRef}>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur eligendi corrupti explicabo nemo quod. Debitis sit eius atque id nulla. Sequi quidem eos obcaecati rem enim quibusdam labore vitae nisi?</p>
+        <>
+            <div className={styles.serviceItem} onMouseOver={(() => {toggleDescription(true)})} onMouseLeave={(() => {toggleDescription(false)})}>
+                <img src={image}/>
+                <div className={styles.itemFooter}>
+                    <span>{title}</span>
+                </div> 
+                <div className={styles.serviceDescriptionContainer} ref={shadowRef}>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur eligendi corrupti explicabo nemo quod. Debitis sit eius atque id nulla. Sequi quidem eos obcaecati rem enim quibusdam labore vitae nisi?</p>
+                    <button onClick={(() => {setShowModal(true)})}>SABER MÁS</button>
+                </div>
             </div>
-        </div>
+            {showModal ? 
+            <div className={styles.modalContainer}>
+                <div className={styles.modalBody}>
+                    <img onClick={(() => {setShowModal(false)})} src='assets/icons/close-icon.png' alt='cerrar'/>
+                    <span>{title}</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus sapiente commodi eaque animi est dolorem accusamus reiciendis deserunt maiores consequuntur veritatis accusantium aspernatur libero natus tempore atque, enim laboriosam sint! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quisquam ullam perspiciatis placeat velit, nobis amet. Suscipit nisi eius labore aspernatur, dolores sint et veniam omnis culpa magni soluta fugiat?</p>
+                </div>
+            </div>
+            :''}
+        </>
     )
 }
 
