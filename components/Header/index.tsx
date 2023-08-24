3814321fefa8808c9d08a17c8282ac61e9b07d42
NVrhovski <neyenvrhovski@gmail.com>
2023-08-24 20:39:35 +0200

    upercase

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -35,12 +35,12 @@ const Header = ({isMobile}: IPropsHeader) => {
                         <img className={styles.navClose} alt='Cerrar' src='assets/icons/close-icon.png' onClick={(() => {toggleNavbar(false)})}/>
                         <nav>
                             <ul className={styles.sections}>
-                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#inicio'}>INICIO</Link></li>
-                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#servicios'}>SERVICIOS</Link></li>
-                                <li onClick={(() => {toggleNavbar(false)})}><Link href={''}>GALERÍA</Link></li>
-                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#reservas'}>RESERVAS</Link></li>
-                                <li onClick={(() => {toggleNavbar(false)})}><Link href={''}>TESTIMONIOS</Link></li>
-                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#contacto'}>CONTACTO</Link></li>
+                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#inicio'}>Inicio</Link></li>
+                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#servicios'}>Servicios</Link></li>
+                                <li onClick={(() => {toggleNavbar(false)})}><Link href={''}>Galería</Link></li>
+                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#reservas'}>Reservas</Link></li>
+                                <li onClick={(() => {toggleNavbar(false)})}><Link href={''}>Testimonios</Link></li>
+                                <li onClick={(() => {toggleNavbar(false)})}><Link href={'#contacto'}>Contacto</Link></li>
                             </ul>
                         </nav>
                     </div>
@@ -48,12 +48,12 @@ const Header = ({isMobile}: IPropsHeader) => {
                 : 
                 <nav>
                     <ul className={styles.sections}>
-                        <li><Link href={'#inicio'}>INICIO</Link></li>
-                        <li><Link href={'#servicios'}>SERVICIOS</Link></li>
-                        <li><Link href={''}>GALERÍA</Link></li>
-                        <li><Link href={'#reservas'}>RESERVAS</Link></li>
-                        <li><Link href={''}>TESTIMONIOS</Link></li>
-                        <li><Link href={'#contacto'}>CONTACTO</Link></li>
+                        <li><Link href={'#inicio'}>Inicio</Link></li>
+                        <li><Link href={'#servicios'}>Servicios</Link></li>
+                        <li><Link href={''}>Galería</Link></li>
+                        <li><Link href={'#reservas'}>Reservas</Link></li>
+                        <li><Link href={''}>Testimonios</Link></li>
+                        <li><Link href={'#contacto'}>Contacto</Link></li>
                     </ul>
                 </nav>}
             </div>
