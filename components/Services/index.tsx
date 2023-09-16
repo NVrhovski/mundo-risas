b78de8ab35fc2a638fef55f28c5a1b9cfae6e1eb
NVrhovski <neyenvrhovski@gmail.com>
2023-09-16 14:34:31 +0200

    si

--- components/Services/index.tsx
+++ components/Services/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import ServicesCard from '../ServicesCard'
 import ServicesDropdown from '../ServicesDropdown'
 import styles from './Services.module.scss'
@@ -9,12 +9,6 @@ interface IPropsServices
 }
 
 const Services = ({isMobile}: IPropsServices) => {
-
-    const responsiveRef = useRef<any>(null);
-
-    useEffect(() => {
-        responsiveRef.current.style.display = 'grid'
-    },[])
 
     return(
         <div className={styles.mainServicesContainer}>
@@ -28,7 +22,7 @@ const Services = ({isMobile}: IPropsServices) => {
                 <ServicesDropdown id={4} title='Egresados / Egresaditos' description='Conmemorá el fin de esta hermosa etapa en nuestro salón de eventos. Ofrecemos deliciosas opciones de catering, además de entretenimiento, como juegos para grandes y chicos, música, baile y entrega de diplomas para que puedas disfrutar de este momento especial con tu familia.'/>
             </div>
             :
-            <div className={styles.servicesGrid} ref={responsiveRef} style={{display: 'none'}}>
+            <div className={styles.servicesGrid}>
                 <ServicesCard id={1} image='/assets/images/infantiles-background.jpg' title='Fiestas infantiles' description='Celebrá el cumpleaños de tus hijos con nosotros. Ofrecemos una amplia variedad de opciones de entretenimiento, desde juegos animados hasta maquillaje y disfraces para los niños más pequeños. También contamos con actividades como escalar, bailar bajo una lluvia de burbujas y un espectacular show de Robot LED.'/>
                 <ServicesCard id={2} image='/assets/images/gallery-2.jpg' title="Fiestas teen's" description='Festejá un cumple inolvidable con tus amigos en un ambiente lleno de diversión, música, baile y una lluvia de burbujas, humo, luces y pulseras que brillan en la oscuridad. Además, contamos con un espectacular show de Robot LED que impresionará a todos tus invitados. Y para calmar la sed, disfrutá de nuestra barra de tragos con jugo y granadina.'/>
                 <ServicesCard id={3} image='/assets/images/bautismos-background.jpg' title='Bautismos / Comuniones' description='Celebrá este día tan importante de tu hijo o hija con nosotros en un ambiente tranquilo y acogedor. Ofrecemos opciones de catering y actividades para que puedan disfrutar de este momento especial con familiares y amigos.'/>
